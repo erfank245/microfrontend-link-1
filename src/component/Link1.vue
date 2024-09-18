20d950f9b0668b864fdb804b01170060ae85b958
erfank245 <erfank245@gmail.com>
2024-09-18 14:56:08 +0330

    update

--- src/component/Link1.vue
+++ src/component/Link1.vue
@@ -1,20 +1,35 @@
 <template>
-    <pre>
-      asndk
-      asdmklaskd
-      samdlkasmd
-      asmdlsakmdl
-      asdm,klsadm
-      asmdlkasmdlkas
-      sadmklsakdl
-      asdkmlaskdl
-    </pre>
-  </template>
-  <script>
-  export default {
-   
-  };
-  </script>
-  <style scoped>
-  
-  </style>
+  <div class="paddnav"></div>
+  <div class="center-items">
+    <div class="text">
+      صفحه تستی شماره ۱ میکرو فرانت اند
+    </div>
+  </div>
+</template>
+<script>
+export default {
+ 
+};
+</script>
+<style scoped>
+  .paddnav {
+    width: 100%;
+    height: 120px;
+  }
+  .center-items {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    margin: auto;
+    width: 50%;
+  }
+  .logo {
+    width: 100%;
+    max-width: 400px;
+    margin: auto;
+  }
+  .text {
+    font-weight: 600;
+  }
+</style>
